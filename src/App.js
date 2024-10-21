8d41dd5c717471ae2d472a3c46384170b4538261
KangJS <asdfqaz1996@naver.com>
2024-10-21 15:03:59 +0900

    Refactor package.json to add react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -1,92 +1,18 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import TodoBoard from "./components/TodoBoard";
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import { useEffect, useState } from "react";
-import api from "./utils/api";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import TodoPage from "./pages/TodoPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [todoValue, setTodoValue] = useState("");
-
-  const getTodo = async () => {
-    const response = await api.get("/todos");
-    setTodoList(response.data.data);
-  };
-
-  useEffect(() => {
-    getTodo();
-  }, []);
-
-  const addTodo = async () => {
-    try {
-      const response = await api.post("/todos", {
-        todo: todoValue,
-        isDone: false,
-      });
-      if (response.status === 200) {
-        setTodoValue("");
-        getTodo();
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const updateItem = async (id) => {
-    try {
-      const todo = todoList.find((item) => item._id === id);
-      const response = await api.put(`/todos/${id}`, {
-        isDone: !todo.isDone,
-      });
-      if (response.status === 200) {
-        getTodo();
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const deleteItem = async (id) => {
-    try {
-      const response = await api.delete(`/todos/${id}`);
-      if (response.status === 200) {
-        getTodo();
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
   return (
-    <Container>
-      <Row className="add-item-row">
-        <Col xs={12} sm={10}>
-          <input
-            type="text"
-            placeholder="할일을 입력하세요"
-            className="input-box"
-            value={todoValue}
-            onChange={(e) => setTodoValue(e.target.value)}
-          />
-        </Col>
-        <Col xs={12} sm={2}>
-          <button className="button-add" onClick={addTodo}>
-            추가
-          </button>
-        </Col>
-      </Row>
+    <Routes>
+      <Route path="/" element={<TodoPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
-      <TodoBoard
-        todoList={todoList}
-        deleteTodo={deleteItem}
-        updateTodo={updateItem}
-      />
-    </Container>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
